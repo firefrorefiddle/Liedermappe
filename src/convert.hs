8884ed403cf76bb255d8f47ddc53bf3c486cf46b
Michael Hartl <hartl@geoinfo.tuwien.ac.at>
2015-02-06 15:43:14 +0100

    converter actually converts. still need solution for chords tight together

--- src/convert.hs
+++ src/convert.hs
@@ -37,3 +37,31 @@ isChordsLine = (>0.8) . truePart . map isChord . words . removeSpecials
                           falses = fromIntegral $ length . filter (==False) $ xs
                       in trues / falses
         removeSpecials = replace (`elem` ['(', ')', '/']) ' '
+
+convertLines [] = []
+convertLines (x:y:xs) | isChordsLine x = insertChords (chordsFromLine x) y :
+                                         convertLines xs
+                      | otherwise = x : convertLines (y:xs)
+
+chordsFromLine l =
+  let idxd = zip [0..] l
+  in scan idxd
+  where isWord = not.isSpace.snd
+        ret w = (fst.head $ w, map snd w)
+        scan il =
+          case dropWhile (not.isWord) il of
+            [] -> []
+            xs -> (ret.takeWord $ xs) : (scan.dropWord $ xs)
+        takeWord = takeWhile isWord
+        dropWord = dropWhile isWord
+
+insertChords cs l = go (reverse cs) l
+  where toChord c = "\\[" ++ c ++ "]"
+        go [] l = l
+        go ((i,c):cs) l = let l' = if length l < i
+                                   then l ++ replicate (i - length l) ' '
+                                   else l
+                              (before, after) = splitAt i l'
+                          in go cs (before ++ toChord c ++ after)
+
+main = interact (unlines . convertLines . lines)
