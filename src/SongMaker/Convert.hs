b50900ed7de49ca4704f8e15f3b777360c5ffdc4
Michael Hartl <hartl@geoinfo.tuwien.ac.at>
2015-02-09 16:47:02 +0100

    first successful conversion

--- src/SongMaker/Convert.hs
+++ src/SongMaker/Convert.hs
@@ -6,14 +6,21 @@ import SongMaker.Common
 import SongMaker.Read
 import SongMaker.Write
 
+import Data.Char
+
 convertStream :: Stream -> Stream
 convertStream s = let (h, ls) = readStream s
                   in (writeHeader h) ++
+                     "\n\\beginverse\n"++
                      (unlines . convertLines $ ls) ++
                      (writeFooter h)
 
 convertLines :: [Line] -> [Line]
 convertLines [] = []
+convertLines (x:xs)   | all isSpace x =
+  case xs of
+    [] -> ["\\endverse"]
+    ys -> ["\\endverse","","\\beginverse"]++convertLines xs
 convertLines (x:y:xs) | isChordsLine x = insertChords (chordsFromLine x) y :
                                          convertLines xs
                       | otherwise = x : convertLines (y:xs)
