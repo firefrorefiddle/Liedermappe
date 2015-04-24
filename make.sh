166a84776d9580bc8bb72014e8c5e59563614892
Michael Hartl <mikehartl17@gmail.com>
2015-04-24 15:16:36 +0200

    add jugendmappe file

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 bin/songmaker lieder
+pdflatex tex/Jugend
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
 mv Gemeinde.pdf out
+mv Jugend.pdf out
 mv Gemeinde_TO.pdf out
 mv Gemeinde_OH.pdf out
 rm -f *.aux *.log
