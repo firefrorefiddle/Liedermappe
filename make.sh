602ca0bd11879e7e948b3429100c64d5b9021312
matinrothauer <martin.rothauer@gmx.at>
2015-04-24 19:22:10 +0200

    Revert "Edit"
    This reverts commit c5f49271f6e445285a0ce1fbf82ac5fc8d406dbe.

--- make.sh
+++ make.sh
@@ -2,13 +2,11 @@
 
 bin/songmaker lieder
 pdflatex tex/Jugend
-pdflatex tex/Jugend_TO
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
 mv Gemeinde.pdf out
 mv Jugend.pdf out
-mv Jugend_TO.pdf out
 mv Gemeinde_TO.pdf out
 mv Gemeinde_OH.pdf out
 rm -f *.aux *.log
