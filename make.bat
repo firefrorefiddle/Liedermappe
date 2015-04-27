08b7a039c09ff905e3a85063cdf49360cfebae8c
Michael Hartl <mikehartl17@gmail.com>
2015-04-27 12:27:33 +0200

    cut down on page margins, remove page number

--- make.bat
+++ make.bat
@@ -2,12 +2,14 @@
 bin/songmaker lieder
 pdflatex tex/Jugend
 pdflatex tex/Jugend_TO
+pdflatex tex/Jugend_OH
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
-move Gemeinde.pdf out
 move Jugend.pdf out
 move Jugend_TO.pdf out
+move Jugend_OH.pdf out
+move Gemeinde.pdf out
 move Gemeinde_TO.pdf out
 move Gemeinde_OH.pdf out
 del -f *.aux *.log
--- make.sh
+++ make.sh
@@ -3,12 +3,14 @@
 bin/songmaker lieder
 pdflatex tex/Jugend
 pdflatex tex/Jugend_TO
+pdflatex tex/Jugend_OH
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
-mv Gemeinde.pdf out
 mv Jugend.pdf out
 mv Jugend_TO.pdf out
+mv Jugend_OH.pdf out
+mv Gemeinde.pdf out
 mv Gemeinde_TO.pdf out
 mv Gemeinde_OH.pdf out
 rm -f *.aux *.log
