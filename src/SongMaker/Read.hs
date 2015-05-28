ac034c36aacb14eac7d1e9317c1fc7be19a1a759
Michael Hartl <mikehartl17@gmail.com>
2015-05-28 15:17:55 +0200

    restructure songmaker, add support for underscores

--- src/SongMaker/Read.hs
+++ src/SongMaker/Read.hs
@@ -10,5 +10,5 @@ import SongMaker.Read.Header
 import SongMaker.Common
 
 readStream :: Stream -> (Header, [Line])
-readStream s = (readHeader s, skipHeader s)
+readStream s = (readHeader s, skipHeader s ++ [""])
 
