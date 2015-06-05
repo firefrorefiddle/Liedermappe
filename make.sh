4980ea24d5e7e853e9d776e5b0b3c392d65403fd
Michael Hartl <mikehartl17@gmail.com>
2015-06-05 10:59:25 +0200

    add inhaltsverzeichnis (indexes)

--- make.sh
+++ make.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 songmaker lieder
+
+
 pdflatex tex/Jugend
 pdflatex tex/Jugend_TO
 pdflatex tex/Jugend_OH
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
+
+songidx inhalt-jugend.sxd inhalt-jugend.sbx
+songidx inhalt-gemeinde.sxd inhalt-gemeinde.sbx
+
+if [ $? -lt 127 ]
+then
+    pdflatex tex/Jugend
+    pdflatex tex/Jugend_TO
+    pdflatex tex/Gemeinde
+    pdflatex tex/Gemeinde_TO
+fi
+
 mv Jugend.pdf out
 mv Jugend_TO.pdf out
 mv Jugend_OH.pdf out
