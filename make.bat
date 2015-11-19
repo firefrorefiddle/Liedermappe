946fe7a65423a8459a00b98c2c3f556b2db281ed
matinrothauer <martin.rothauer@gmx.at>
2015-11-19 12:00:38 +0100

    eigene_mappe eingefügt

--- make.bat
+++ make.bat
@@ -6,10 +6,16 @@ pdflatex tex/Jugend_OH
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Gemeinde_OH
+pdflatex tex/Eigene_Mappe
+pdflatex tex/Eigene_Mappe_TO
+pdflatex tex/Eigene_Mappe_OH
 move Jugend.pdf out
 move Jugend_TO.pdf out
 move Jugend_OH.pdf out
 move Gemeinde.pdf out
 move Gemeinde_TO.pdf out
 move Gemeinde_OH.pdf out
+move Eigene_Mappe.pdf out
+move Eigene_Mappe_TO.pdf out
+move Eigene_Mappe_OH.pdf out
 del -f *.aux *.log
