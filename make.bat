4c60338c985bb48d9d29b1934f234edea1d8bd16
matinrothauer <martin.rothauer@gmx.at>
2016-04-12 17:03:01 +0200

    Correct Indexes
    Correct Indexes

--- make.bat
+++ make.bat
@@ -1,21 +1,18 @@
 
+songidx inhalt-jugend.sxd inhalt-jugend.sbx
+songidx inhalt-gemeinde.sxd inhalt-gemeinde.sbx
+songidx inhalt-eigene.sxd inhalt-eigene.sbx
+
 songmaker lieder
+
 pdflatex tex/Jugend
 pdflatex tex/Jugend_TO
-pdflatex tex/Jugend_OH
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
-pdflatex tex/Gemeinde_OH
 pdflatex tex/Eigene_Mappe
-pdflatex tex/Eigene_Mappe_TO
-pdflatex tex/Eigene_Mappe_OH
 move Jugend.pdf out
 move Jugend_TO.pdf out
-move Jugend_OH.pdf out
 move Gemeinde.pdf out
 move Gemeinde_TO.pdf out
-move Gemeinde_OH.pdf out
 move Eigene_Mappe.pdf out
-move Eigene_Mappe_TO.pdf out
-move Eigene_Mappe_OH.pdf out
 del -f *.aux *.log
