7728f13fe07345d08b21c53745c455051ce5f534
matinrothauer <martin.rothauer@gmx.at>
2016-06-03 22:46:02 +0200

    Liedermappe neu
    Liedermappe neu

--- make.bat
+++ make.bat
@@ -2,6 +2,8 @@
 songidx inhalt-jugend.sxd inhalt-jugend.sbx
 songidx inhalt-gemeinde.sxd inhalt-gemeinde.sbx
 songidx inhalt-eigene.sxd inhalt-eigene.sbx
+songidx inhalt-eigene.sxd inhalt-eigene-2.sbx
+songidx inhalt-eigene.sxd inhalt-eigene-3.sbx
 
 songmaker lieder
 
@@ -10,9 +12,13 @@ pdflatex tex/Jugend_TO
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
 pdflatex tex/Eigene_Mappe
+pdflatex tex/Eigene_Mappe_2
+pdflatex tex/Eigene_Mappe_3
 move Jugend.pdf out
 move Jugend_TO.pdf out
 move Gemeinde.pdf out
 move Gemeinde_TO.pdf out
 move Eigene_Mappe.pdf out
+move Eigene_Mappe_2.pdf out
+move Eigene_Mappe_3.pdf out
 del -f *.aux *.log
