57e1051e2d7b1d4dbdf19a5fb5d61bcd2bd2fbcd
matinrothauer <martin.rothauer@gmx.at>
2016-12-09 15:30:03 +0100

    Akkorde gesetzt
    Gott zeigt mir den Weg

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 
 songidx inhalt-jugend.sxd inhalt-jugend.sbx
 songidx inhalt-gemeinde.sxd inhalt-gemeinde.sbx
-rem songidx inhalt-eigene.sxd inhalt-eigene.sbx
+songidx inhalt-eigene.sxd inhalt-eigene.sbx
 songidx inhalt-eigene-2.sxd inhalt-eigene-2.sbx
 songidx inhalt-eigene-3.sxd inhalt-eigene-3.sbx
 
@@ -11,14 +11,14 @@ pdflatex tex/Jugend
 pdflatex tex/Jugend_TO
 pdflatex tex/Gemeinde
 pdflatex tex/Gemeinde_TO
-rem pdflatex tex/Eigene_Mappe
+pdflatex tex/Eigene_Mappe
 pdflatex tex/Eigene_Mappe_2
 pdflatex tex/Eigene_Mappe_3
 move Jugend.pdf out
 move Jugend_TO.pdf out
 move Gemeinde.pdf out
 move Gemeinde_TO.pdf out
-rem move Eigene_Mappe.pdf out
+move Eigene_Mappe.pdf out
 move Eigene_Mappe_2.pdf out
 move Eigene_Mappe_3.pdf out
 del -f *.aux *.log
